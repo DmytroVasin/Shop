c1d59e053d809bfe64510641bcae5a33bbc07aad
Vasin Dmitriy <vasindima779@gmail.com>
2013-02-10 18:06:57 +0200

    Added menu

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,46 @@
 $(function (){
-	
+	$('#sdt_menu > li').bind('mouseenter',function(){
+		var $elem = $(this);
+		$elem.find('img')
+		.stop(true)
+		.animate({
+			'width':'164px',
+			'height':'55px',
+			'left':'0px'
+		},300,'easeOutBack')
+		.andSelf()
+		.find('.sdt_wrap')
+		.stop(true)
+		.animate({'top':'70px'},400,'easeOutBack')
+		.andSelf()
+		.find('.sdt_descr')
+		.css('display', 'block')
+		.andSelf()
+		.find('.sdt_active')
+		.stop(true)
+		.animate({'height':'80px'},300,function(){
+			var $sub_menu = $elem.find('.sdt_box');
+			$sub_menu.slideDown(600);
+		});
+	}).bind('mouseleave',function(){
+		var $elem = $(this);
+		var $sub_menu = $elem.find('.sdt_box');
+		if($sub_menu.length)
+			$sub_menu.hide();
+		$elem.find('.sdt_active')
+		.stop(true)
+		.animate({'height':'0px'},200)
+		.andSelf().find('img')
+		.stop(true)
+		.animate({
+			'width':'0px',
+			'height':'0px',
+			'left':'82px'},300)
+		.andSelf()
+		.find('.sdt_wrap')
+		.stop(true)
+		.animate({'top':'11px'},500)
+		.find('.sdt_descr')
+		.css('display', 'none');
+	});
 });
