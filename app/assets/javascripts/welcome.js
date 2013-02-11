8f2d3d47cd360cb9668a58d994f00291fd1d5b51
Vasin Dmitriy <vasindima779@gmail.com>
2013-02-11 23:57:31 +0200

    Added => "Our best sellers" part 1

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -54,30 +54,6 @@ $(function (){
   });
 
 // ------------------------------------------ Slider
-
-	// $('#slides').slides({
-	// 	preload: true,
-	// 	preloadImage: 'images/loading.gif',
-	// 	play: 5000,
-	// 	pause: 2500,
-	// 	hoverPause: true,
-	// 	animationStart: function(current){
-	// 		$('.caption').animate({
-	// 			bottom:-35
-	// 		},100);
-	// 	},
-	// 	animationComplete: function(current){
-	// 		$('.caption').animate({
-	// 			bottom:0
-	// 		},200);
-	// 	},
-	// 	slidesLoaded: function() {
-	// 		$('.caption').animate({
-	// 			bottom:0
-	// 		},200);
-	// 	}
-	// });
-
 	theRotator();
 
 });
