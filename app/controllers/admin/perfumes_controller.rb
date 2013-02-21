513ddf9c7a90658528511c33e4da77310b5486bd
Vasin Dmitriy <vasindima779@gmail.com>
2013-02-21 10:26:15 +0200

    Added Perfume_controller ( Create, Update, Destroy actions ), added category displayin in show page

--- app/controllers/admin/perfumes_controller.rb
+++ app/controllers/admin/perfumes_controller.rb
@@ -10,10 +10,39 @@ class Admin::PerfumesController < Admin::BaseController
   end
 
   def new
-     @perfume = Perfume.new
+    @perfume = Perfume.new
+  end
+
+  def create
+    @perfume = Perfume.new(params[:perfume])
+
+    if @perfume.save
+      flash[:notice] = "Pefume created!"
+      redirect_to admin_perfume_path(@perfume.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
     @perfume = Perfume.find(params[:id])
+  end
+
+  def update
+    @perfume = Perfume.find(params[:id])
+
+     if @perfume.update_attributes(params[:perfume])
+      flash[:notice] = "Perfume updated"
+      redirect_to admin_perfume_path(@perfume.id)
+     else
+      render 'edit'
+     end
+  end
+
+  def destroy
+    @perfume = Perfume.find(params[:id])
+
+    @perfume.destroy
+    redirect_to admin_perfumes_path
   end
 end
--- app/models/perfume.rb
+++ app/models/perfume.rb
@@ -1,5 +1,5 @@
 class Perfume < ActiveRecord::Base
-  attr_accessible :description, :price, :title, :in_stock
+  attr_accessible :description, :price, :title, :in_stock, :category_ids
 
   has_and_belongs_to_many :categories
 end
