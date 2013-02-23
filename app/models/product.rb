c730bc6dd4066d334a561e88703eac5f60976150
Vasin Dmitriy <vasindima779@gmail.com>
2013-02-23 12:44:50 +0200

    Added validations for product model, added in_stock and price field, and error.css.scss file for custom style for errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :title, :in_stock, :category_ids
 
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :in_stock, inclusion: { in: [true, false] }
+  validates :title, length: { minimum: 10 }
+  validates :description, length: { minimum: 100 }
+
   has_and_belongs_to_many :categories
 end
