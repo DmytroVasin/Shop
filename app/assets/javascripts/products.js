e8f1e788781b742b6548158ee561fef0163d0e5b
Vasin Dmitriy <vasindima779@gmail.com>
2013-02-24 19:42:01 +0200

    Added form_tag and removed js ( without submiting on change )

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,14 +2,14 @@ $(function () {
 
 // Sort menu - dropdown list
   $('.sortdropdown').on('change', function () {
-    window.location='/products?sort='+ this.value;
+//    window.location='/products?sort='+ this.value;
   });
 
 
 // Sort Section PRICE list
   var countChecked = function() {
     $(this).parent().siblings().find('input[type=checkbox]:checked').prop('checked', false);
-    window.location='/products?price='+ this.value;
+//    window.location='/products?price='+ this.value;
   };
 
   $( 'input[name=price_checkboxes]' ).on( "click", function(){
