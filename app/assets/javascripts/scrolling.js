4417c22863cf0e2fff723c18f215c4abd451e3f8
Vasin Dmitriy <vasindima779@gmail.com>
2013-03-02 11:18:51 +0200

    Added scroll top - after click view all

--- app/assets/javascripts/scrolling.js
+++ app/assets/javascripts/scrolling.js
@@ -1,7 +1,16 @@
 $(function () {
 //  Scrolling page aftee loading
+
+  if(window.location.hash == '#brands') {
+    $("body,html").animate({"scrollTop":470},"slow");
+  }
+
   $('.viewallbrandslink').on('click', function(){
-    window.location='/products';
-    $('body').scrollTo(350)
+    if (window.location.pathname == '/products') {
+      $("body,html").animate({"scrollTop":470},"slow");
+    } else {
+      window.location='/products#brands';
+    }
+
   });
 });
