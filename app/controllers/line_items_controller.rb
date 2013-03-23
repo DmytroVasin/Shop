854ca6443875bdb8621e9488950dfb53f74c575d
Vasin Dmitriy <vasindima779@gmail.com>
2013-03-23 20:33:17 +0200

    Removed excess action for carts, line_items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,44 +1,4 @@
 class LineItemsController < ApplicationController
-  # GET /line_items
-  # GET /line_items.json
-  def index
-    @line_items = LineItem.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @line_items }
-    end
-  end
-
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show
-    @line_item = LineItem.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @line_item }
-    end
-  end
-
-  # GET /line_items/new
-  # GET /line_items/new.json
-  def new
-    @line_item = LineItem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @line_item }
-    end
-  end
-
-  # GET /line_items/1/edit
-  def edit
-    @line_item = LineItem.find(params[:id])
-  end
-
-  # POST /line_items
-  # POST /line_items.json
   def create
     @cart      = current_cart
     product    = Product.find(params[:product_id])
@@ -53,24 +13,8 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PUT /line_items/1
-  # PUT /line_items/1.json
-  def update
-    @line_item = LineItem.find(params[:id])
-
-    respond_to do |format|
-      if @line_item.update_attributes(params[:line_item])
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { head :no_content }
-      end
-    end
-  end
-
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
   def destroy
     @cart = current_cart
-
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
 
@@ -85,19 +29,15 @@ class LineItemsController < ApplicationController
     end
   end
 
-
   def decrease
     @cart      = current_cart
     @line_item = @cart.decrease(params[:id])
-
-    @items = @cart.line_items.order('created_at ASC')
+    @items     = @cart.line_items.order('created_at ASC')
 
     respond_to do |format|
       if @items.empty?
-
         @cart.destroy
         session[:cart_id] = nil
-
         format.html { redirect_to products_path, notice: 'Empty cart' }
       else
         if @line_item.save
@@ -111,9 +51,7 @@ class LineItemsController < ApplicationController
   def increase
     @cart      = current_cart
     @line_item = @cart.increase(params[:id])
-
-    @items = @cart.line_items.order('created_at ASC')
-
+    @items     = @cart.line_items.order('created_at ASC')
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully updated.' }
