c743cae630c8b2673ede54f21d52c701691448c7
Vasin Dmitriy <vasindima779@gmail.com>
2013-03-24 17:33:53 +0200

    Added associations AND simple validations

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,5 @@ class LineItem < ActiveRecord::Base
 
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,12 @@
 class Order < ActiveRecord::Base
-  attr_accessible :address, :email, :name, :pay_type, :phone
-
-  validates :terms_of_service, acceptance: true
+  attr_accessible :address, :email, :name, :surname, :pay_type, :phone, :additional_phone, :region, :city, :info
 
   PAYMENT_TYPES = ["New poshta", "Ukr poshta"]
+
+  has_many :line_items, dependent: :destroy
+
+  validates :terms_of_service, acceptance: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+  validates :address, :email, :name, :surname, :phone, :region, :city, presence: true
+
 end
