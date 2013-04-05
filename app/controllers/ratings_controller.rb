eb16d809da5d5f426be717c1daa9ad1d61930b63
Vasin Dmitriy <vasindima779@gmail.com>
2013-04-05 11:18:39 +0300

    Create partial and controller for action NEW CREATE

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,9 @@
+class RatingsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404060303) do
+ActiveRecord::Schema.define(:version => 20130405075858) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",              :default => "", :null => false
@@ -94,6 +94,13 @@ ActiveRecord::Schema.define(:version => 20130404060303) do
     t.datetime "created_at",                                                   :null => false
     t.datetime "updated_at",                                                   :null => false
     t.integer  "rank",                                      :default => 0
+  end
+
+  create_table "ratings", :force => true do |t|
+    t.string   "point"
+    t.string   "ip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
