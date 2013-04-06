cd7369e37a8528de2818b9f2d7350321b2000801
Vasin Dmitriy <vasindima779@gmail.com>
2013-04-06 12:59:49 +0300

    Updated create method for Vote (in welcome page)

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,15 +1,26 @@
 class RatingsController < ApplicationController
+
   def create
-    point = params[:rating][:point].to_i
-    if ( 1 <= point ) && ( point <= 7)
-      @rating = Rating.new
-      @rating.point = point
-      @rating.save
-      flash[:notice] = 'Rating saved'
-      redirect_to root_path
+    point       = params[:rating][:point].to_i
+    point_value = (1 <= point) && (point <= 7) ? true : false
+    remote_ip   = request.remote_ip.to_s
+
+    if point_value
+      @rating = Rating.new({ point: point, ip: remote_ip })
+
+      if @rating.save
+        flash[:notice] = 'Data Accepted'
+      else
+        error_flash
+      end
     else
-      flash[:alert] = 'Incorrect value'
-      redirect_to root_path
+      error_flash
     end
+    redirect_to root_path
+  end
+
+  private
+  def error_flash
+    flash[:alert] = 'Incorrect value'
   end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,5 @@
 class Rating < ActiveRecord::Base
   attr_accessible :ip, :point
+
+  validates :ip, uniqueness: true
 end
