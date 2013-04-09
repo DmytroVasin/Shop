43512709125a6fa10ccf1fe1499483c91b3f37d8
Vasin Dmitriy <vasindima779@gmail.com>
2013-04-09 22:47:33 +0300

    Added products from BD to rotator in Root page ( welcome/index )

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,12 @@
 class WelcomeController < ApplicationController
   def index
     # BRANDS for menu
-    brands           = Brand.joins(:products)
-    @brands_left     = brands.random_by_id_shuffle(10)
-    @brands_right    = brands.random_by_id_shuffle(10)
+    brands        = Brand.joins(:products)
+    @brands_left  = brands.random_by_id_shuffle(10)
+    @brands_right = brands.random_by_id_shuffle(10)
 
     # Rating menu ( Vote menu )
-    @ratings         = Rating.new
+    @ratings      = Rating.new
 
     ratings          = Rating.select(:point)
     @count           = ratings.count
@@ -15,10 +15,17 @@ class WelcomeController < ApplicationController
     @hash_with_counts, @hash_with_percentege = vote_rank(hash_with_points, @count)
 
     # New parfume's - for her and him
-    products = Product.order('created_at ASC')
-    for_him = products.by_category_name('For Him').limit(2)
-    for_her = products.by_category_name('For Her').limit(2)
 
-    @for_them = for_her + for_him
+    products          = Product.where(in_stock: true)
+
+    products_for_them = products.order('created_at ASC')
+    for_him           = products_for_them.by_category_name('For Him').limit(2)
+    for_her           = products_for_them.by_category_name('For Her').limit(2)
+
+    @for_them         = for_her + for_him
+
+    # Our best sellers
+    @best_sellers = products.order('rank DESC').limit(6)
+
   end
 end
