f48e4650a76db83f740ccb370c855e86902c93d8
Vasin Dmitriy <vasindima779@gmail.com>
2013-10-19 13:01:37 +0300

    changed style, remove product selection js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,32 +1,5 @@
 $(function () {
 
-// Sort menu - dropdown list
-  $('.sortdropdown').on('change', function () {
-    $('#form_sort').submit();
-  });
-
-
-// Sort Section PRICE list
-  var countChecked = function () {
-    $(this).parent().parent().siblings().find('input[type=checkbox]:checked').prop('checked', false);
-
-    $('#form_sort').submit();
-  };
-
-  $('input[name=price_between]').on("click", function () {
-    countChecked.call(this);
-  });
-
-//  Sort Section PRODUCT TYPE list
-  $('input[name=product_type]').on("click", function () {
-    countChecked.call(this);
-  });
-
-//  Sort Section BRAND TYPE list
-  $('input[name=brand_type]').on("click", function () {
-    countChecked.call(this);
-  });
-
 // ENDLESS paginator     -------------------------------------------------------------
   $('#ednless_paginator').on('click', function () {
 
