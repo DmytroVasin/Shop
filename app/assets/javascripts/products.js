cdc3ef1e4e1e4f65d9cab6437207341d8613a98f
Vasin Dmitriy <vasindima779@gmail.com>
2013-11-09 11:17:09 +0200

    Intermidiate commit

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,32 +1,93 @@
 $(function () {
-  $("#zooming").elevateZoom({ zoomWindowPosition: 2, zoomWindowOffetx: 35 });
+	$("#zooming").elevateZoom({ zoomWindowPosition: 2, zoomWindowOffetx: 35 });
 
 	$('.video_slider').on('click', function(){
 		$('.iframe_video_wrapper').stop(true).slideToggle('show');
 		$(this).toggleClass('border_bottom');
 	});
 
-	$(".image_polaroid img").elevateZoom({gallery:'wrap_image_rotator', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true, loadingIcon: '/assets/progres.gif'}); 
+	$(".image_polaroid img").elevateZoom({gallery:'wrap_image_rotator', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true, loadingIcon: '/assets/progres.gif'});
 
-	$(".image_polaroid img").bind("click", function(e) {  
-	  var ez =   $('.image_polaroid img').data('elevateZoom');	
+	$(".image_polaroid img").bind("click", function(e) {
+		var ez =   $('.image_polaroid img').data('elevateZoom');
 		$.fancybox(ez.getGalleryList());
-	  return false;
+		return false;
 	});
 
 
-// // ENDLESS paginator     -------------------------------------------------------------
-//   $('#ednless_paginator').on('click', function () {
+	// // ENDLESS paginator     -------------------------------------------------------------
+	//   $('#ednless_paginator').on('click', function () {
 
-//     url = $('.pagination .next_page').attr('href');
-//     if (url) {
-//       $.getScript(url);
-//       $('.pagination ul').replaceWith('<div id="endless_message"></div>');
-//     }
-//   });
-// // ENDLESS pagination ( remove adding link - if count of products == 6 )
-//   if (!($('div').hasClass('pagination'))) {
-//     $('#ednless_paginator').remove();
-//   }
-// // ENDLESS pagination END  ----------------------------------------------------------
+	//     url = $('.pagination .next_page').attr('href');
+	//     if (url) {
+	//       $.getScript(url);
+	//       $('.pagination ul').replaceWith('<div id="endless_message"></div>');
+	//     }
+	//   });
+	// // ENDLESS pagination ( remove adding link - if count of products == 6 )
+	//   if (!($('div').hasClass('pagination'))) {
+	//     $('#ednless_paginator').remove();
+	//   }
+	// // ENDLESS pagination END  ----------------------------------------------------------
+
+
+
+
+	// // Sort menu - dropdown list
+	// $('.sortdropdown').on('change', function () {
+	// 	$('#form_sort').submit();
+	// });
+
+	// // Sort Section PRICE list
+	// var countChecked = function () {
+	// 	$(this).parent().parent().siblings().find('input[type=checkbox]:checked').prop('checked', false);
+
+	// 	$('#form_sort').submit();
+	// };
+
+	// $('input[name=price_between]').on("click", function () {
+	// 	countChecked.call(this);
+	// });
+
+  function getParam(sParamName){
+    var Params = location.search.substring(1).split("&");
+    var variable = "";
+    for (var i = 0; i < Params.length; i++){
+      if (Params[i].split("=")[0] == sParamName){
+        if (Params[i].split("=").length > 1) variable = Params[i].split("=")[1];
+        return variable;
+      }
+    }
+    return "";
+  };
+
+  console.log( getParam("filter") );
+
+
+
+	// Submit form in every change:
+	$('body').on('change', ".sortdropdown, input[type=checkbox]", function () {
+		var prices = $('.price_bar input[type=checkbox]:checked');
+		var prices_params = [];
+
+		$.each(prices, function(){
+			prices_params.push($(this).val());
+		})
+
+		$.ajax({
+		  type: "GET",
+		  url: '/products.js',
+		  data: { price_between: prices_params }
+		})
+
+
+
+
+
+
+		// $('#form_sort').submit();
+		// var value = $(this).val();
+		// window.location.search = 'filter='+prices_params;
+	});
+
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,8 @@
 class ProductsController < ApplicationController
 
   def index
+    @flag = true;
+
     @price_hash = hash_of_prices
     @sort_hash = hash_of_sort
     @sort_method_array = @sort_hash.values
@@ -12,25 +14,55 @@ class ProductsController < ApplicationController
     @colors  = Color.joins(:products).group('colors.id').order('name ASC')
 
 
-    @products = Product.order('created_at DESC').page(params[:page]).per(12)
+    @products = Product.order('created_at DESC')
 
-    if @sort_method_array.include? params[:sort_by]
-      @products = Product.send(params[:sort_by]).page(params[:page]).per(12)
+    # if @sort_method_array.include? params[:sort_by]
+      # @products = Product.send(params[:sort_by]).page(params[:page]).per(12)
       #TODO: in this scope we use order - thats why we use Product model rather than @product
+    # end
+
+
+    price_between_array = params[:price_between]
+    price_between_array.each do |pb|
+      if @price_hash.include? pb
+        arr = pb.split(':')
+        arr[1] = '10000' if arr[1].nil? # TODO: 10000 - this is max price ( how to remove it? or let it stay ?)
+
+        @products = @products.price_between(arr[0], arr[1])
+      end
     end
 
-    if @price_hash.include? params[:price_between]
-      arr = params[:price_between].split(':')
-      arr[1] = '10000' if arr[1].nil? # TODO: 10000 - this is max price ( how to remove it? or let it stay ?)
 
-      @products = @products.price_between(arr[0], arr[1])
-    end
 
-    if @brands.map(&:name).include? params[:brand_type]
-      @products = @products.by_brands_name(params[:brand_type])
-    end
+
+
+    # if @brands.map(&:name).include? params[:brand_type]
+    #   @products = @products.by_brands_name(params[:brand_type])
+    # end
+
+
+
+
+
+
+    # if @colors.include? params[:sort_by]
+
+
+
+
+
+    Rails.logger.info '>>>'*50
+    Rails.logger.info @products.count
+    Rails.logger.info '<<<'*50
+
+    @products = @products.page(params[:page]).per(12)
 
     session[:products_params] = params
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
@@ -41,19 +73,19 @@ class ProductsController < ApplicationController
 
   private
 
-    def hash_of_prices
-      { '0:150'   => '< 150',
-        '150:200' => '150 < 200',
-        '200:350' => '200 < 350',
-        '350:500' => '350 < 500',
-        '500:700' => '500 < 700',
-        '700'     => '700+' }
-    end
+  def hash_of_prices
+    { '0:150'   => '< 150',
+      '150:200' => '150 < 200',
+      '200:350' => '200 < 350',
+      '350:500' => '350 < 500',
+      '500:700' => '500 < 700',
+      '700'     => '700+' }
+  end
 
-    def hash_of_sort
-      { 'Лучшее'    => 'best',
-        'Цена low'  => 'low',
-        'Цена high' => 'high',
-        'Новое'     => 'newest' }
-    end
+  def hash_of_sort
+    { 'Лучшее'    => 'best',
+      'Цена low'  => 'low',
+      'Цена high' => 'high',
+      'Новое'     => 'newest' }
+  end
 end
