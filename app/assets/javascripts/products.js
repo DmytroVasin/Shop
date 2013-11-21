0f32ff55e3623f45785252dd7fc618a7885e6930
Vasin Dmitriy <vasindima779@gmail.com>
2013-11-21 21:57:45 +0200

    Added sorting by price

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -26,8 +26,38 @@ $(function () {
 	  });
 	// ENDLESS pagination END  ----------------------------------------------------------
 
+	// Throttle method from underscore.js
+	throttle = function(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    options || (options = {});
+    console.log(options);
+    var later = function() {
+      previous = options.leading === false ? 0 : new Date;
+      timeout = null;
+      result = func.apply(context, args);
+    };
+    return function() {
+      var now = new Date;
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0) {
+        clearTimeout(timeout);
+        timeout = null;
+        previous = now;
+        result = func.apply(context, args);
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
+
 	// Submit form in every change:
-	$('body').on('change', ".sortdropdown, input:checkbox", function () {
+	changeMeter = function () {
 		var categories = $('.categories_bar input[type=checkbox]:checked');
 		var brands     = $('.brand_bar input[type=checkbox]:checked');
 		var gender     = $('.gender_bar input[type=checkbox]:checked');
@@ -44,19 +74,19 @@ $(function () {
 			return Array($('#leftValue').val(), $('#rightValue').val());
 		};
 
-		$.ajax({
-			type: "GET",
-			url: '/products.js',
-			data: {
-				categories_params: getValues(categories),
-				brands_params: getValues(brands),
-				gender_params: getValues(gender),
-				color_params: getValues(color),
-				price_between: getPrice,
-				sort_direction: select_val,
-				flag: true
-			}
-		});
-	});
-
+    $.ajax({
+	    type: "GET",
+	    url: '/products.js',
+	    data: {
+	      categories_params: getValues(categories),
+	      brands_params: getValues(brands),
+	      gender_params: getValues(gender),
+	      color_params: getValues(color),
+	      price_between: getPrice,
+	      sort_direction: select_val,
+	      flag: true
+      }
+	  })
+	};
+	$('body').on('change', ".sortdropdown, input:checkbox, #leftValue, #rightValue", throttle(changeMeter, 3000, {leading: false}));
 });
