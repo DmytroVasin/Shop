9b88a2dd0e3ca8cac66c155f22e19905e3ee49b3
Vasin Dmitriy <vasindima779@gmail.com>
2013-12-08 13:02:26 +0200

    small changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,11 @@ class Review < ActiveRecord::Base
   attr_accessible :full_name, :address, :url_id, :delivery_time, :quality, :suggestions
 
   before_validation :url_id_is_exist
+  
   def url_id_is_exist
   	self.url_id = self.url_id.split('/')[-1].gsub(/\D/, '').to_i
-  	if Product.pluck(:id).include?( self.url_id )
-  	else
+
+  	unless Product.pluck(:id).include?( self.url_id )
   		errors.add(:url_id,'mistake')
   	end
   end
