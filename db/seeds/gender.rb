a668e5a53edb0dc179375a3e8898c3b1db2c3591
Vasin Dmitriy <vasindima779@gmail.com>
2014-04-03 12:38:51 +0300

    Increase of Value in Price scroll

--- db/seeds/gender.rb
+++ db/seeds/gender.rb
@@ -1,5 +1,5 @@
 def add_gender
   {:'Unisex'=> 'Унисекс', :'Womens' => 'Женское', :'Mens'=> 'Мужское'}.each do |type, rus|
-    Gender.first_or_create({ gender: type, gender_rus: rus })
+    Gender.create({ gender: type, gender_rus: rus })
   end
 end
