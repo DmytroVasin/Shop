fce0cd435db7d8e5aefd2f3267084c16c3713c8c
Vasin Dmitriy <vasindima779@gmail.com>
2014-04-05 10:36:28 +0300

    Added Cont with data for new fileds and translate it to RUS

--- app/models/colour.rb
+++ app/models/colour.rb
@@ -36,6 +36,71 @@ class Colour < ActiveRecord::Base
     ['Голубой', 'BlueGay'],
   ]
 
+  MATERIAL = [
+    ['Кожа', 'skin'],
+    ['Полиэстер', 'polyester'],
+    ['Искусственная кожа', 'imitation_leather'],
+    ['Нейлон', 'nylon'],
+    ['Синтетика', 'synthetics'],
+    ['Хлопок', 'cotton'],
+    ['Брезент', 'tarpaulin'],
+    ['Полиуретан', 'polyurethane'],
+    ['Сетка', 'net'],
+    ['Рипстоп', 'ripstop'],
+    ['Винил', 'vinyl'],
+    ['Атласный', 'satin'],
+    ['Замша', 'suede'],
+    ['Лакированная кожа', 'patent_leather'],
+    ['Жаккард', 'jacquard'],
+    ['Пластиковая', 'plastic'],
+    ['Шерсть', 'wool'],
+    ['Саржа', 'twill'],
+    ['Лен', 'flax'],
+    ['Соломенный', 'straw'],
+    ['Конопляный', 'cannabic'],
+    ['Искусственная замша', 'faux suede'],
+    ['Трикотажный', 'knitted'],
+    ['Шелк', 'silk'],
+    ['Шерсть', 'wool'],
+    ['Деним', 'denim'],
+    ['Зернистая кожа', 'grainy_leather'],
+    ['Резиновый', 'rubber'],
+    ['Рафия', 'raffia'],
+    ['Кожа ягненка', 'lamb_leather'],
+    ['Искусственный шелк', 'rayon'],
+    ['Змеиная кожа', 'snakeskin'],
+    ['Джут', 'jute'],
+    ['Микрофирба', 'mikrofirba'],
+    ['Брезент', 'tarpaulin'],
+    ['Войлок', 'felt'],
+    ['Неопрен', 'neoprene'],
+    ['Нубук', 'nubuck'],
+    ['Овчина', 'sheepskin'],
+    ['Твид', 'tweed'],
+    ['Вельвет', 'corduroy'],
+    ['Вышивка', 'embroidery'],
+    ['Бриллиантовый', 'diamond'],
+    ['Кружева', 'lace'],
+    ['Полипропилен', 'polypropylene'],
+    ['Ширлинг', 'shearling'],
+    ['Силикон', 'silicone'],
+    ['Вискоз', 'viscose']
+  ]
+
+  CLOSING_TYPE = [
+    ['Молния', 'zipper'],
+    ['Защелка', 'latch'],
+    ['Магнит', 'magnet'],
+    ['Пряжка', 'buckle'],
+    ['Поворотный замок', 'cam_lock'],
+    ['Шнурок', 'lace'],
+    ['Кнопка с фиксацией', 'button_with_fixation'],
+    ['Пружинный замок', 'spring_lock'],
+    ['Липучка', 'velcro'],
+    ['Защелка', 'latch'],
+    ['Кнопка', 'button']
+  ]
+
   def self.create_hash_by(array)
     Hash[array.map { |rus_color| [COMMON_COLORS_RUS.assoc(rus_color).last, rus_color ] }]
   end
