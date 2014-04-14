2dc2eeeb2a5d0d12834d4b72c5946f82a6b72e07
Vasin Dmitriy <vasindima779@gmail.com>
2014-04-14 12:41:35 +0300

    Added correct migration ( for migrate DB )

--- db/migrate/20140401163343_add_hstore_common_color_to_colours.rb
+++ db/migrate/20140401163343_add_hstore_common_color_to_colours.rb
@@ -1,30 +1,15 @@
 class AddHstoreCommonColorToColours < ActiveRecord::Migration
   def self.up
     add_column :colours, :common_colors, :hstore
-    add_column :colours, :materials, :hstore
-    add_column :colours, :zippers, :hstore
-    add_column :colours, :features, :hstore
+    add_column :products, :materials, :hstore
+    add_column :products, :zippers, :hstore
+    add_column :products, :features, :hstore
   end
+
   def self.down
     remove_column :colours, :common_colors
-    remove_column :colours, :materials
-    remove_column :colours, :zippers
-    remove_column :colours, :features
+    remove_column :products, :materials
+    remove_column :products, :zippers
+    remove_column :products, :features
   end
 end
-
-# class AddHstoreCommonColorToColours < ActiveRecord::Migration
-#   def self.up
-#     add_column :colours, :common_colors, :hstore
-#     add_column :products, :materials, :hstore
-#     add_column :products, :zippers, :hstore
-#     add_column :products, :features, :hstore
-#   end
-
-#   def self.down
-#     remove_column :colours, :common_colors
-#     remove_column :products, :materials
-#     remove_column :products, :zippers
-#     remove_column :products, :features
-#   end
-# end
--- db/migrate/20140401163845_add_hstore_index_to_colours.rb
+++ db/migrate/20140401163845_add_hstore_index_to_colours.rb
@@ -1,31 +1,15 @@
 class AddHstoreIndexToColours < ActiveRecord::Migration
   def self.up
     add_hstore_index :colours, :common_colors
-    add_hstore_index :colours, :materials
-    add_hstore_index :colours, :zippers
-    add_hstore_index :colours, :features
+    add_hstore_index :products, :materials
+    add_hstore_index :products, :zippers
+    add_hstore_index :products, :features
   end
 
   def self.down
     remove_hstore_index :colours, :common_colors
-    remove_hstore_index :colours, :materials
-    remove_hstore_index :colours, :zippers
-    remove_hstore_index :colours, :features
+    remove_hstore_index :products, :materials
+    remove_hstore_index :products, :zippers
+    remove_hstore_index :products, :features
   end
 end
-
-# class AddHstoreIndexToColours < ActiveRecord::Migration
-#   def self.up
-#     add_hstore_index :colours, :common_colors
-#     add_hstore_index :products, :materials
-#     add_hstore_index :products, :zippers
-#     add_hstore_index :products, :features
-#   end
-
-#   def self.down
-#     remove_hstore_index :colours, :common_colors
-#     remove_hstore_index :products, :materials
-#     remove_hstore_index :products, :zippers
-#     remove_hstore_index :products, :features
-#   end
-# end
