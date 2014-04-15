50ced6a945d7002eafb143d89bf402928400bc1c
Vasin Dmitriy <vasindima779@gmail.com>
2014-04-15 12:57:55 +0300

    remove validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,3 @@
 class Review < ActiveRecord::Base
   attr_accessible :full_name, :address, :url_id, :delivery_time, :quality, :suggestions
-
-  before_validation :url_id_is_exist
-  
-  def url_id_is_exist
-  	self.url_id = self.url_id.split('/')[-1].gsub(/\D/, '').to_i
-
-  	unless Product.pluck(:id).include?( self.url_id )
-  		errors.add(:url_id,'mistake')
-  	end
-  end
 end
