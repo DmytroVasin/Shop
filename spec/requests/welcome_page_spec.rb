ef38670f1bc1e443cdde51f8e0c45a1c9f8d6b13
Vasin Dmitriy <vasindima779@gmail.com>
2014-04-22 12:11:08 +0300

    Simple start ( just working rspec spec/requests... command

---
+++ spec/requests/welcome_page_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'Welcome page', js: true do
+  # let(:deal_name) { 'DHH' }
+
+  before :each do
+    visit home_page
+  end
+
+  # context 'Dashboard' do
+    # before do
+      # sign_in_as user
+    # end
+
+    it 'openes correctly' do
+      page.should have_content 'Оригинальные брендовые сумки из США'
+      screenshot_and_open_image
+    end
+  # end
+end
