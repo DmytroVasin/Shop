0ad08881b241151e0bd3caade64e89c9c751a0dd
Vasin Dmitriy <vasindima779@gmail.com>
2014-04-24 12:56:27 +0300

    Insert map to solve problem with multiple optional colors in one color state

--- app/models/colour/colour_scopes.rb
+++ app/models/colour/colour_scopes.rb
@@ -2,7 +2,8 @@ module Colour::ColourScopes
   def self.included(base)
     base.class_eval do
       def self.get_uniq_common_colors
-        pluck(:common_colors).reduce({}, :merge)
+        # Insert .map to solve the problem - if one color get many optional colors
+        pluck(:common_colors).map{|hash| Hash[*hash.first] }.reduce({}, :merge)
       end
     end
   end
